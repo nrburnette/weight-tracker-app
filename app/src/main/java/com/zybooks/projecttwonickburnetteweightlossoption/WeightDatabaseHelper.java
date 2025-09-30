d2341187397d72e874839dc64d32884cb662b843
Nick Burnette <nick.burnette@snhu.edu>
2025-09-30 14:31:21 -0700

    WIP Artifact 3 DB Room setup with no-op migration

--- app/src/main/java/com/zybooks/projecttwonickburnetteweightlossoption/WeightDatabaseHelper.java
+++ app/src/main/java/com/zybooks/projecttwonickburnetteweightlossoption/WeightDatabaseHelper.java
@@ -3,10 +3,8 @@ package com.zybooks.projecttwonickburnetteweightlossoption;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-// import android.database.SQLException; // FIXME delete
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-// import android.util.Log; // FIXME delete
 
 import java.util.ArrayList;
 
